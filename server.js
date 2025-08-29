a9a4bbb9ca0e4f75c28c23a006375b78a338cfdf
badboytermenator23 <dziopaabends@hotmail.com>
2025-08-28 21:00:07 -0500

    Add comments for clarity and update route message

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+// Middleware để phân tích dữ liệu JSON và URL-encoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route gốc
 app.get('/', (req, res) => {
-    res.send('Chào mừng đến với ứng dụng tương tác bài viết mạng xã hội!');
+    res.send('Chào mừng đến với ứng dụng tương tác bài viết mạng xã hội!
+    Hãy chia sẻ bài viết của bạn!'); // Cập nhật thông điệp chào mừng
 });
 
 // Khởi chạy server
